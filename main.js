91e0b3ee835f70ce9f3b4f693f9f10803c0c17c5
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-06-12 09:57:28 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let mevalar = ["olma", "anor", "banan", "apelsin" ]
+
+
+mevalar.push('uzum')
+mevalar.pop()
+mevalar.pop()
+
+mevalar.unshift(1,2,3) 
+
+console.log(mevalar);
+
+mevalar.pop()
+
+mevalar.shift()
+
+console.log(mevalar);
